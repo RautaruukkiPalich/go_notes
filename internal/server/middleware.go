d3280d9c64d22d6f8fff5391991fbb277dac6bd3
palich <rautaruukkipalich@gmail.com>
2024-03-19 19:51:48 +0500

    some fix

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -8,45 +8,46 @@ import (
 	"net/http"
 	"strings"
 	"time"
+
+	"github.com/rautaruukkipalich/go_notes/internal/model"
 )
 
 var ErrInvalidToken = errors.New("invalid token")
 
-type UserInfo struct{
-	ID int `json:"id"`
-	TokenTTL time.Time `json:"token_ttl"`
-	Valid bool `json:"is_valid"`
-}
 
 
-var mockUserInfo = &UserInfo{
+
+var mockUserInfo = &model.User{
 	ID: 1,
-	TokenTTL: time.Time(time.Now().UTC().Add(time.Second*15)),
+	TokenTTL: time.Time(time.Now().UTC().Add(time.Minute*10)),
 	Valid: true,
 }
 
-func getUserInfoByToken(token string) (*UserInfo, error) {
+func getUserInfoByToken(token string) (*model.User, error) {
 	user := mockUserInfo
-	user.TokenTTL = time.Now().UTC().Add(time.Second*15)
+	// if token expires -> request to sso -> get new token
+	// user.TokenTTL = time.Now().UTC().Add(time.Second*15)
 	return user, nil
 }
 
-func validateUserData(user *UserInfo) error {
+func validateUserData(user *model.User) error {
 	if user.ID == 0 {return ErrInvalidToken}
 	if !user.Valid {return ErrInvalidToken}
-	if user.TokenTTL.Unix() < time.Now().UTC().Unix() {return ErrInvalidToken}
+	// if user.TokenTTL.Unix() < time.Now().UTC().Unix() {return ErrInvalidToken}
 	return nil
 }
 
-func (s *Server) RedisGetUser(token string) (*UserInfo, error) {
+func (s *Server) RedisGetUser(token string) (*model.User, error) {
 	s.logger.Info(fmt.Sprintf("redis get %s", token))
 	data, err := s.cache.User().Get(token)
 
 	if err != nil {
+		fmt.Printf("%T", err)
+		s.logger.Errorf("redis err: %v", err)
 		return nil, err
 	}
 
-	var user UserInfo
+	var user model.User
 	err = json.Unmarshal(data, &user)
 	if err != nil {
 		return nil, fmt.Errorf("nil data")
@@ -55,8 +56,8 @@ func (s *Server) RedisGetUser(token string) (*UserInfo, error) {
 	return &user, nil
 }
 
-func (s *Server) RedisSetUser(token string, user *UserInfo) error{
-	s.logger.Info(fmt.Sprintf("redis set %s", token))
+func (s *Server) RedisSetUser(token string, user *model.User) error{
+	s.logger.Info(fmt.Sprintf("redis set %s, %v", token, user.TokenTTL))
 	data, err := json.Marshal(user)
 	if err != nil {
 		return err
@@ -71,7 +72,6 @@ func (s *Server) AuthMiddleware(next http.Handler) http.Handler{
 			authHeaderChunks := strings.Split(authHeader, " ")
 
 			if len(authHeaderChunks) != 2 {
-				// s.error(w, r, errorResponse{Error: ErrInvalidToken.Error(), Code: http.StatusForbidden})
 				s.logger.Info("user is not authorized")
 				next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), UserKey, 0)))
 				return
@@ -84,21 +84,21 @@ func (s *Server) AuthMiddleware(next http.Handler) http.Handler{
 				user, err = getUserInfoByToken(token)
 				// user.TokenTTL = time.Now().UTC().Add(time.Duration(time.Second*15))
 				if err != nil {
-					s.logger.Error(fmt.Sprintf("get user info b token: %v", err.Error()))
+					s.logger.Errorf("get user info b token: %v", err)
 					s.error(w, r, errorResponse{Error: ErrInternalServerError.Error(), Code: http.StatusInternalServerError})
 					return
 				}
+
 				//validate user
 				if err := validateUserData(user); err != nil {
-					s.logger.Error(fmt.Sprintf("get user info b token: %v", err.Error()))
+					s.logger.Errorf("get user info b token: %v", err)
 					s.error(w, r, errorResponse{Error: ErrInvalidToken.Error(), Code: http.StatusForbidden})
 					return
 				}
+				
 				//set redis
 				if err := s.RedisSetUser(token, user); err != nil{
-					s.logger.Error(fmt.Sprintf("get user info b token: %v", err.Error()))
-					// s.error(w, r, errorResponse{Error: ErrInvalidToken.Error(), Code: http.StatusForbidden})
-					// return
+					s.logger.Errorf("redis: set user err: %v", err)
 				}
 			} 
 			
