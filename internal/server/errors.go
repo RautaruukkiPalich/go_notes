56833cfaf0671e481730385e7deeb2a895ec475d
palich <rautaruukkipalich@gmail.com>
2024-03-19 19:52:09 +0500

    add error

--- internal/server/errors.go
+++ internal/server/errors.go
@@ -4,6 +4,7 @@ import "fmt"
 
 var (
 	ErrParseURL            = fmt.Errorf("url parse error")
+	ErrUnprocessableEntity = fmt.Errorf("data parse error")
 	ErrNoPermissons        = fmt.Errorf("no permissions")
 	ErrNotFound            = fmt.Errorf("not found")
 	ErrInternalServerError = fmt.Errorf("internal error")
