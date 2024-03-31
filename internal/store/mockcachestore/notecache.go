8bd3d61e76e365ac549cbec73be7c2aa3108127d
palich <rautaruukkipalich@gmail.com>
2024-04-01 01:54:43 +0500

    add cashe mocks

--- internal/store/mockcachestore/notecache.go
+++ internal/store/mockcachestore/notecache.go
@@ -1,19 +1,45 @@
 package mockcachestore
 
-import "github.com/rautaruukkipalich/go_notes/internal/model"
+import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+
+	"github.com/rautaruukkipalich/go_notes/internal/model"
+)
 
 type NoteCache struct {
-	client map[string]any
+	client map[string][]byte
 }
 
 func newNoteCache() *NoteCache {
 	return &NoteCache{
-		client: make(map[string]any),
+		client: make(map[string][]byte),
 	}
 }
 
-func (n *NoteCache) Get()                               {}
-func (n *NoteCache) GetNoteById(int)(model.Note, error) { return model.Note{}, nil}
-func (n *NoteCache) Set(*model.Note) error              { return nil }
+func (n *NoteCache) Get() {}
+
+func (n *NoteCache) GetNoteById(id int)(model.Note, error) { 
+	var note model.Note
+
+	key := fmt.Sprintf("note:%d", id)
+	data:= n.client[key]
+	if err := json.Unmarshal([]byte(data), &note); err != nil {
+		return note, err
+	}
+	return note, nil
+}
+
+func (n *NoteCache) Set(note *model.Note) error { 
+	id := "note:"+strconv.Itoa(note.ID)
+	json_data, err := json.Marshal(&note)
+	if err != nil {
+		return err
+	}
+	n.client[id] = json_data
+	return nil
+}
+
 func (n *NoteCache) SetNotes([]model.Note) error		{ return nil }
 func (n *NoteCache) Delete(int) error					{ return nil}
--- internal/store/mockcachestore/usercache.go
+++ internal/store/mockcachestore/usercache.go
@@ -1,23 +1,33 @@
 package mockcachestore
 
-import "time"
+import (
+	"errors"
+	"time"
+)
 
 type UserCache struct {
-	client map[string]any
+	client map[string][]byte
 }
 
 func newUserCache() *UserCache {
 	return &UserCache{
-		client: make(map[string]any),
+		client: make(map[string][]byte),
 	}
 }
 
 func (u *UserCache) Get(token string) ([]byte, error) {
-	return nil, nil
+	return u.client[token], nil
 }
+
 func (u *UserCache) Set(token string, data []byte, expire time.Time) error {
+	ttl := expire.Sub(time.Now().UTC())
+	if ttl < 0 {
+		return errors.New("token expired")
+	}
+	u.client[token] = data
 	return nil
 }
 func (u *UserCache) Delete(token string) error {
+	delete(u.client, token)
 	return nil
 }
